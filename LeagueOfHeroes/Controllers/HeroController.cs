b7b582d1e6ecde4628f9c3f858df4324619a17ec
Alexis Juarez <ajuarezdev@outlook.com>
2020-06-19 17:12:23 -0500

    *HeroController -Route change
    *Ratings view
    -new layout for heroes list items
    
    *styles added to match header bar

--- LeagueOfHeroes/Controllers/HeroController.cs
+++ LeagueOfHeroes/Controllers/HeroController.cs
@@ -15,80 +15,21 @@ namespace LeagueOfHeroes.Controllers
         {
             _heroService = heroService;
         }
-        // GET: HomeController1
+
+        [Route("heroes")]
         public async Task<ActionResult> Index()
         {
-            var heroes = await _heroService.GetAllHeroesAsync();
-            return View(heroes);
-        }
-
-        // GET: HomeController1/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: HomeController1/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: HomeController1/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var heroes = await _heroService.GetAllHeroesAsync();
+                return View(heroes);
             }
-            catch
+            catch (Exception)
             {
-                return View();
-            }
-        }
 
-        // GET: HomeController1/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: HomeController1/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
+                throw;
             }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomeController1/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: HomeController1/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            
         }
     }
 }
